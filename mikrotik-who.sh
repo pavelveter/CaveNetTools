4a5c26f75cea95a43bf2979bf86bf23c8a4fe136
Borisov Pavel <kayjaylitee@gmail.com>
2022-08-04 02:02:55 +0300

    Added IP check for whoip.sh

--- mikrotik-who.sh
+++ mikrotik-who.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -o errexit
 set -o nounset
@@ -8,7 +8,7 @@ readonly mikrotik=192.168.88.1
 readonly port=2200
 readonly user=admin
 
-vendict="$(dirname "$0")"/oui.txt
+readonly vendict=$(dirname $0)/oui.txt
 
 readonly c_violet="e[35m"
 readonly c_green="e[32m"
@@ -22,22 +22,23 @@ readonly wot="${c_red}WAITING${c_reset}  "
 [[ -s ${vendict} ]] || (echo -e "Wait a minute, downloading database of vendors...\n"; \
                         wget http://standards.ieee.org/develop/regauth/oui/oui.txt)
 
-while read -r line; do
+while read line; do
     if [[ ${line} == *${unknown}* ]]; then 
-        vendor=$(grep "$(echo "${line}" | tr -s " " | cut -d " " -f3 \
-                | cut -d "-" -f1,2,3)" "${vendict}" | cut -c 19-)
+        vendor=$(grep $(echo ${line} | tr -s " " | cut -d " " -f3 \
+                | cut -d "-" -f1,2,3) ${vendict} | cut -c 19-)
 
-        echo -e ${line//e\[/\\e[} by "${vendor}"
+        echo -e ${line//e\[/\\e[} by ${vendor}
     else
         echo -e ${line//e\[/\\e[}
     fi
-done <<< "$( \
+done <<< $( \
     ssh ${user}@${mikrotik} -p ${port} 'ip dhcp-server lease print' \
     | awk 'FIELDWIDTHS="5 15 4 17 1 4" { \
-           if (index($4, ":")) { \
-               ip=$2; mac=$4; gsub(":", "-", mac); \
-               cnt=substr($0, 66, 2);  gsub("b", "'${cnc}'", cnt); gsub("w", "'${wot}'", cnt); \
-               if (index(t, ";")) { rem=substr(t, 10, length(t)-10) } else { rem="'${unknown}'" } \
-               printf "%s %s %s   %s\n", ip, cnt, mac, rem } \
-               t=$0; }'
-)"
+            if (index($4, ":")) { \
+                ip=$2; mac=$4; gsub(":", "-", mac); \
+                cnt=substr($0, 66, 2);  gsub("b", "'${cnc}'", cnt); gsub("w", "'${wot}'", cnt); \
+                if (index(t, ";")) { rem=substr(t, 10, length(t)-10) } else { rem="'${unknown}'" } \
+                printf "%s %s %s   %s\n", ip, cnt, mac, rem
+            } \
+        t=$0; }' \
+)
--- whoip.sh
+++ whoip.sh
@@ -4,50 +4,57 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly CountryCodes=$(dirname $0)/country_codes.txt
-readonly CountryFlags=$(dirname $0)/country_flags.txt
+CountryCodes="$(dirname "$0")/country_codes.txt"
+readonly CountryCodes
+CountryFlags="$(dirname "$0")/country_flags.txt"
+readonly CountryFlags
 readonly GoogleMaps="google.com/maps/place/"
 readonly url="https://ipinfo.io"
 
 # Divider for for
 IFS=";"
 
-response=$(curl -s ${url}/${1})
+if ! [ 1 = ${#} ] || ! [[ ${1} =~ ^(0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){3}0*(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))$ ]]; then
+    echo "ERROR! We need valid IP address as first argument"
+    exit 1
+fi
 
-echo ${response} | grep '"bogon": true' > /dev/null && (echo "\033[31mCan't find anything about\033[0m ${1}"; exit) 
-echo ${response} | grep 'Wrong ip' > /dev/null && (echo "\033[31mWrong ip-address\033[0m ${1}"; exit) 
+response=$(curl -s "${url}/${1}")
+
+echo "${response}" | grep '"bogon": true' > /dev/null && (echo "ERROR! Can't find anything about ${1}"; exit) 
+echo "${response}" | grep 'Wrong ip' > /dev/null && (echo "ERROR! Wrong ip-address ${1}"; exit) 
 
 r=""
 i=0
 
-for s in $(echo ${response} | jq -r ".country, .ip, .city, .region, .org, .timezone, .loc" | tr "\n" ";"); do
+for s in $(echo "${response}" | jq -r ".country, .ip, .city, .region, .org, .timezone, .loc" | tr "\n" ";"); do
 
     # Add flag and full name of country
-    if (( 0 == $i )); then
-        r+=$(grep \#$(grep $s ${CountryCodes} | cut -d "," -f1) ${CountryFlags} | cut -d "#" -f1)
+    if [ 0 == $i ]; then
+        r+=$(grep "$(grep "$s" "${CountryCodes}" | cut -d "," -f1)" "${CountryFlags}" | cut -d "#" -f1)
         r+=" #"
-        r+=$(grep $s ${CountryCodes} | cut -d "," -f1)"#"
+        r+=$(grep "$s" "${CountryCodes}" | cut -d "," -f1)"#"
     fi
 
     # Trying to add domain name
     ret=0
-    nslookup ${1} > /dev/null || ret=1
+    nslookup "${1}" > /dev/null || ret=1
 
-    if (( 0 == ${ret} && 1 == $i )); then
+    if [ 0 == ${ret} ] && [ 1 == $i ]; then
         r+=$s"#"
         s=""
-        r+=$(nslookup ${1} | grep name | cut -d "=" -f2 | sed -e "s/^.//; s/.$//")
+        r+=$(nslookup "${1}" | grep name | cut -d "=" -f2 | sed -e "s/^.//; s/.$//")
     fi
 
     # Add URL for location
-    if (( 6 == $i )); then
+    if [ 6 == $i ]; then
         r+=${GoogleMaps}
     fi
 
-    let i+=1
+    i+=1
     r+=$s"#"
 done
 
-echo $r | sed "s/#/;/g; s/;null;/;Unknown;/g" # | pygmentize -O style=paraiso-dark -l awk
+echo $r | sed "s/#/;/g; s/;null;/;Unknown;/g"
 
 IFS=" "
